56eaf9f8f5fe6a06fdc0b22bdae4d28f91827bda
Kade-Yoo <yhm9207@naver.com>
2024-01-05 07:54:33 +0900

    Spring Security + JWT Practice

---
+++ README.md
@@ -0,0 +1,36 @@
+# 과제
+
+## 환경
+- Spring Boot 3.2.1
+- Kotlin 1.9.21
+- ORM(JPA)
+- 인증(Spring Security + JWT)
+
+## 구성
+### Architecture
+- Clean Architecture
+    - Multiple Module 구성
+
+### 인증
+- Spring Security + JWT
+    - SecurityConfig(Spring Security 설정)
+    - Jwt (토큰 정보)
+    - JwtAuthenticationFilter (토큰 인증 필터)
+    - JwtProvider (토큰 생성, 토큰 정보)
+
+### CRUD API
+- 인증 로직을 구현하여 CRUD API는 회원과 관련된 API로 작성
+
+| Method | API URI      | 내용                                      |
+|--------|--------------|-----------------------------------------|
+| POST   | /login       | 로그인 API                                 |
+| POST   | /join        | 회원 가입 API                               |
+| PUT    | /join        | 회원 정보 수정 API                            |
+| DELETE | /join        | 회원 탈퇴 API                               |
+| GET    | /member/list | 회원 정보 전체 조회 API("ADMIN" 권한일 떄만 조회 가능)   |
+| GET    | /member      | 회원 정보 조회 API("ADMIN", "USER" 일 떄 조회 가능) |
+
+## 개선 필요한 점
+- 토큰이 만료되었을 때 refreshToken을 확인하여 accessToken 재발급
+- Test Code 추가 필요
+- 다양한 Exception을 Handling할 수 있도록 개선 필요
---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "3.2.1" apply false
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.21"
+    kotlin("plugin.spring") version "1.9.21" apply false
+    kotlin("plugin.jpa") version "1.9.21" apply false
+}
+
+
+allprojects {
+    group = "com.example.auth"
+    version = "0.0.1-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply {
+        plugin("kotlin")
+        plugin("org.jetbrains.kotlin.jvm")
+        plugin("org.springframework.boot")
+        plugin("io.spring.dependency-management")
+        plugin("org.jetbrains.kotlin.plugin.spring")
+    }
+
+    dependencies {
+        implementation(kotlin("reflect"))
+        implementation(kotlin("stdlib"))
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "17"
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+}
